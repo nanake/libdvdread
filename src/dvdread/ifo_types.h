4fff244127fec84c5b91a4a86dfd2d162056c2ce
Kacper Michajłow <kasper93@gmail.com>
2026-07-29 20:00:56 +0200

    ifo_types: use attribute packing whenever available
    __attribute__((packed)) has been supported since GCC 2.95 (1999), so
    the version check is long obsolete. Also check for __clang__
    explicitly for clang targeting a *-windows-msvc triple.

--- src/dvdread/ifo_types.h
+++ src/dvdread/ifo_types.h
@@ -28,11 +28,9 @@
 
 #undef ATTRIBUTE_PACKED
 
-#if defined(__GNUC__)
-# if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95)
-#  define ATTRIBUTE_PACKED __attribute__ ((packed))
-#  define PRAGMA_PACK 0
-# endif
+#if defined(__GNUC__) || defined(__clang__)
+# define ATTRIBUTE_PACKED __attribute__ ((packed))
+# define PRAGMA_PACK 0
 #endif
 
 #if !defined(ATTRIBUTE_PACKED)
