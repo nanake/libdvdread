15238fc3bb411f8face08b50537da4b64641f1e9
Kacper Michajłow <kasper93@gmail.com>
2026-07-30 11:58:23 +0200

    bswap: remove system specific byte swap functions
    These branches predate compiler byteswap builtins and by now are
    redundant. Most if not all of those platform specific functions, are
    expand to the same builtins on any current system, so they added mostly
    noise. For majority of deployments GCC/Clang/MSVC(-like) compilers the
    builtin is used, for the rest portable C version, which modern
    optimizers lower to a single bswap instruction anyway.
    
    Exotic compilers that fail to recognize the byteswap idiom will emit a
    few extra ALU ops, which is an acceptable trade-off for the
    simplification. They probably wouldn't compile with meson anyway...

--- src/bswap.h
+++ src/bswap.h
@@ -22,79 +22,27 @@
 #ifndef LIBDVDREAD_BSWAP_H
 #define LIBDVDREAD_BSWAP_H
 
-#include <config.h>
-
 #if defined(WORDS_BIGENDIAN)
 /* All bigendian systems are fine, just ignore the swaps. */
 #define B2N_16(x) (void)(x)
 #define B2N_32(x) (void)(x)
 #define B2N_64(x) (void)(x)
 
-#else /* WORDS_BIGENDIAN */
+#elif defined(__GNUC__) || defined(__clang__)
 
-#if defined(__GNUC__) || defined(__clang__)
-
-#define B2N_16(x) x = __builtin_bswap16(x)
-#define B2N_32(x) x = __builtin_bswap32(x)
-#define B2N_64(x) x = __builtin_bswap64(x)
+#define B2N_16(x) ((x) = __builtin_bswap16(x))
+#define B2N_32(x) ((x) = __builtin_bswap32(x))
+#define B2N_64(x) ((x) = __builtin_bswap64(x))
 
 #elif defined(_MSC_VER)
 
 #include <stdlib.h>
-#define B2N_16(x) x = _byteswap_ushort(x)
-#define B2N_32(x) x = _byteswap_ulong(x)
-#define B2N_64(x) x = _byteswap_uint64(x)
+
+#define B2N_16(x) ((x) = _byteswap_ushort(x))
+#define B2N_32(x) ((x) = _byteswap_ulong(x))
+#define B2N_64(x) ((x) = _byteswap_uint64(x))
 
 #else
-
-/* For __FreeBSD_version */
-#if defined(HAVE_SYS_PARAM_H)
-#include <sys/param.h>
-#endif
-
-#if defined(__linux__) || defined(__GLIBC__)
-#include <byteswap.h>
-#define B2N_16(x) x = bswap_16(x)
-#define B2N_32(x) x = bswap_32(x)
-#define B2N_64(x) x = bswap_64(x)
-
-#elif defined(__APPLE__)
-#include <libkern/OSByteOrder.h>
-#define B2N_16(x) x = OSSwapBigToHostInt16(x)
-#define B2N_32(x) x = OSSwapBigToHostInt32(x)
-#define B2N_64(x) x = OSSwapBigToHostInt64(x)
-
-#elif defined(__NetBSD__)
-#include <sys/endian.h>
-#define B2N_16(x) BE16TOH(x)
-#define B2N_32(x) BE32TOH(x)
-#define B2N_64(x) BE64TOH(x)
-
-#elif defined(__OpenBSD__)
-#include <sys/endian.h>
-#define B2N_16(x) x = swap16(x)
-#define B2N_32(x) x = swap32(x)
-#define B2N_64(x) x = swap64(x)
-
-#elif defined(__FreeBSD__) && __FreeBSD_version >= 470000
-#include <sys/endian.h>
-#define B2N_16(x) x = be16toh(x)
-#define B2N_32(x) x = be32toh(x)
-#define B2N_64(x) x = be64toh(x)
-
-#elif defined(__QNXNTO__)
-#include <gulliver.h>
-#define B2N_16(x) x = ENDIAN_RET16(x)
-#define B2N_32(x) x = ENDIAN_RET32(x)
-#define B2N_64(x) x = ENDIAN_RET64(x)
-
-#elif defined(__DragonFly__)
-#include <sys/endian.h>
-#define B2N_16(x) x = bswap16(x)
-#define B2N_32(x) x = bswap32(x)
-#define B2N_64(x) x = bswap64(x)
-
-#elif defined(__FreeBSD__) || defined(__sun) || defined(__bsdi__) || defined(_WIN32) || defined(__CYGWIN__) || defined(__BEOS__) || defined(__OS2__)
 
 #include <stdint.h>
 
@@ -114,17 +62,6 @@ static inline uint64_t dvdread_bswap64(uint64_t x) { return BSWAP64(x); }
 #undef BSWAP32
 #undef BSWAP64
 
-#else
-
-/* If there isn't a header provided with your system with this functionality
- * add the relevant || define( ) to the portable implementation above.
- */
-#error "You need to add endian swap macros for your system"
-
 #endif
-
-#endif
-
-#endif /* WORDS_BIGENDIAN */
 
 #endif /* LIBDVDREAD_BSWAP_H */
